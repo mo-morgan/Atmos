211d6311b239c918509e94b6c323d57aca939510
Morgan Mo <morganandfacebook19@gmail.com>
2020-01-12 06:03:40 -0800

    Im tired, pushed some messy code

---
+++ app/src/main/java/com/example/atmos/adapter/ConfigurationAdapter.java
@@ -0,0 +1,11 @@
+//package com.example.atmos.adapter;
+//
+//
+//import android.support.v7.widget.RecyclerView;
+//
+//public class ConfigurationAdapter extends RecyclerView.Adapter<ConfigurationAdapter.ViewHolder> {
+//
+//    public class ViewHolder extends RecyclerView.ViewHolder {
+//
+//    }
+//}
